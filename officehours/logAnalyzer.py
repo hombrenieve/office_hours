34b6eca0b26aef4ea03bb1cdc5e3c2d2bf4768da
Pablo Rebaque <pablo.rebaque.diaz@ericsson.com>
2020-04-25 17:01:29 +0200

    Refactoring - Create Report class

--- officehours/logAnalyzer.py
+++ officehours/logAnalyzer.py
@@ -4,60 +4,60 @@ import json
 
 logdatefmt="%Y/%m/%d-%H:%M"
 
-def lineToTimepoint(line):
-    lineSplit = line.split(" ")
-    return (datetime.strptime(lineSplit[0], logdatefmt), 
-            lineSplit[1] == "Start" or lineSplit[1] == "Unlock")
+class Report:
+    def lineToTimepoint(self, line):
+        lineSplit = line.split(" ")
+        return (datetime.strptime(lineSplit[0], logdatefmt), 
+                lineSplit[1] == "Start" or lineSplit[1] == "Unlock")
 
-def deltaToStr(tdelta):
-    hours, rem = divmod(tdelta.seconds, 3600)
-    minutes, rem = divmod(rem, 60)
-    return "{:02d}:{:02d}".format(hours, minutes)
+    def deltaToStr(self, tdelta):
+        hours, rem = divmod(tdelta.seconds, 3600)
+        minutes, rem = divmod(rem, 60)
+        return "{:02d}:{:02d}".format(hours, minutes)
 
-def dayHoursToStr(dTime):
-    return dTime.strftime("%H:%M")
+    def dayHoursToStr(self, dTime):
+        return dTime.strftime("%H:%M")
 
-def now():
-    return datetime.now()
+    def now(self):
+        return datetime.now()
 
-def report(lines):
-    data = {}
-    working = timedelta()
-    resting = timedelta()
+    def report(self, lines):
+        data = {}
+        working = timedelta()
+        resting = timedelta()
 
-    if(lines != None and len(lines) > 0):
-        tp = lineToTimepoint(lines[0].rstrip())
-        start = tp[0]
-        for line in lines[1:]:
-            newTP = lineToTimepoint(line.rstrip())
-            delta = newTP[0]-tp[0]
+        if(lines != None and len(lines) > 0):
+            tp = self.lineToTimepoint(lines[0].rstrip())
+            start = tp[0]
+            for line in lines[1:]:
+                newTP = self.lineToTimepoint(line.rstrip())
+                delta = newTP[0]-tp[0]
+                if tp[1]:
+                    working += delta
+                else:
+                    resting += delta
+                tp = newTP
+
             if tp[1]:
+                #fake timepoint
+                tpf = self.now()
+                delta = tpf - tp[0]
                 working += delta
+                end = tpf
             else:
-                resting += delta
-            tp = newTP
+                end = tp[0]
 
-        if tp[1]:
-            #fake timepoint
-            tpf = now()
-            delta = tpf - tp[0]
-            working += delta
-            end = tpf
-        else:
-            end = tp[0]
-
-        data['start'] = dayHoursToStr(start)
-        data['end'] = dayHoursToStr(end)
-        total = end-start
-        if(total.days > 0):
-            raise ValueError("Time account exceeds a day")
-        data['total'] = deltaToStr(total)
-        data['working'] = deltaToStr(working)
-        data['resting'] = deltaToStr(resting)
-    return json.dumps(data)
-
+            data['start'] = self.dayHoursToStr(start)
+            data['end'] = self.dayHoursToStr(end)
+            total = end-start
+            if(total.days > 0):
+                raise ValueError("Time account exceeds a day")
+            data['total'] = self.deltaToStr(total)
+            data['working'] = self.deltaToStr(working)
+            data['resting'] = self.deltaToStr(resting)
+        return json.dumps(data)
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as infile:
         lines = infile.readlines()
-    print(report(lines))
+    print(Report().report(lines))
