146fcb1987f2ebe1c9145ba1e3c5b17ccf409564
Pablo Rebaque <pablo.rebaque.diaz@ericsson.com>
2020-04-25 17:10:59 +0200

    Refactor - lines are member

--- officehours/logAnalyzer.py
+++ officehours/logAnalyzer.py
@@ -5,32 +5,36 @@ import json
 logdatefmt="%Y/%m/%d-%H:%M"
 
 class Report:
-    def lineToTimepoint(self, line):
+
+    def __init__(self, lines):
+        self._lines = lines
+
+    def _lineToTimepoint(self, line):
         lineSplit = line.split(" ")
         return (datetime.strptime(lineSplit[0], logdatefmt), 
                 lineSplit[1] == "Start" or lineSplit[1] == "Unlock")
 
-    def deltaToStr(self, tdelta):
+    def _deltaToStr(self, tdelta):
         hours, rem = divmod(tdelta.seconds, 3600)
         minutes, rem = divmod(rem, 60)
         return "{:02d}:{:02d}".format(hours, minutes)
 
-    def dayHoursToStr(self, dTime):
+    def _dayHoursToStr(self, dTime):
         return dTime.strftime("%H:%M")
 
     def now(self):
         return datetime.now()
 
-    def report(self, lines):
+    def report(self):
         data = {}
         working = timedelta()
         resting = timedelta()
 
-        if(lines != None and len(lines) > 0):
-            tp = self.lineToTimepoint(lines[0].rstrip())
+        if(self._lines != None and len(self._lines) > 0):
+            tp = self._lineToTimepoint(self._lines[0].rstrip())
             start = tp[0]
-            for line in lines[1:]:
-                newTP = self.lineToTimepoint(line.rstrip())
+            for line in self._lines[1:]:
+                newTP = self._lineToTimepoint(line.rstrip())
                 delta = newTP[0]-tp[0]
                 if tp[1]:
                     working += delta
@@ -47,17 +51,17 @@ class Report:
             else:
                 end = tp[0]
 
-            data['start'] = self.dayHoursToStr(start)
-            data['end'] = self.dayHoursToStr(end)
+            data['start'] = self._dayHoursToStr(start)
+            data['end'] = self._dayHoursToStr(end)
             total = end-start
             if(total.days > 0):
                 raise ValueError("Time account exceeds a day")
-            data['total'] = self.deltaToStr(total)
-            data['working'] = self.deltaToStr(working)
-            data['resting'] = self.deltaToStr(resting)
+            data['total'] = self._deltaToStr(total)
+            data['working'] = self._deltaToStr(working)
+            data['resting'] = self._deltaToStr(resting)
         return json.dumps(data)
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as infile:
         lines = infile.readlines()
-    print(Report().report(lines))
+    print(Report(lines).report())
--- ut/test_logAnalyzer.py
+++ ut/test_logAnalyzer.py
@@ -73,65 +73,65 @@ class ReportBuilder:
 class TestLogAnalyzer(unittest.TestCase):
 
     def test_report_isEmptyInput(self):
-        self.assertEqual(logAnalyzer.Report().report([]), "{}", "Should be empty json string")
+        self.assertEqual(logAnalyzer.Report([]).report(), "{}", "Should be empty json string")
 
     def test_report_noPauses(self):
         logs = LogBuilder().start().end().build()
         report = ReportBuilder().working("09:30").resting("00:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
     
     def test_report_onePause(self):
         logs = LogBuilder().start().pause("10:00", "11:00").end().build()
         report = ReportBuilder().working("08:30").resting("01:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
 
     def test_report_severalPauses(self):
         logs = LogBuilder().start().pause("10:00", "10:30").pause("13:15", "13:45").end().build()
         report = ReportBuilder().working("08:30").resting("01:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
     
     def test_report_severalSessionsSameDay(self):
         logs = LogBuilder().start().end("10:00").start("11:00").end().build()
         report = ReportBuilder().working("08:30").resting("01:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
 
     def test_report_severalSessionsWithPausesSameDay(self):
         logs = LogBuilder().start().pause("10:00", "10:30").end("10:35").start("11:05").pause("11:10", "11:40").end().build()
         report = ReportBuilder().working("08:00").resting("01:30").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
     
     def test_report_allDayWorkingWithACrashWhileWorking(self):
         logs = LogBuilder().start().start("11:00").end().build()
         report = ReportBuilder().working("09:30").resting("00:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
     
     def test_report_allDayWorkingWithPauseWithACrashWhileResting(self):
         logs = LogBuilder().start().newLine("11:00", "Lock").start("12:00").end().build()
         report = ReportBuilder().working("08:30").resting("01:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
 
     def test_report_longDayWorking(self):
         logs = LogBuilder().start("09:00", "2020/04/10").end("08:00", "2020/04/11").build()
         report = ReportBuilder().start("09:00").end("08:00").total("23:00").working("23:00").resting("00:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
 
     def test_report_moreThanADayWorking(self):
         logs = LogBuilder().start("09:00", "2020/04/10").end("18:00", "2020/04/11").build()
         with self.assertRaises(ValueError) as context:
-            logAnalyzer.Report().report(logs)
+            logAnalyzer.Report(logs).report()
         self.assertTrue('Time account exceeds a day' in str(context.exception))
 
     @mock.patch('officehours.logAnalyzer.Report.now', side_effect=defaultEnd)
     def test_checkCurrentDay(self, now_function):
         logs = LogBuilder().start().build()
         report = report = ReportBuilder().working("09:30").resting("00:00").build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
     
     @mock.patch('officehours.logAnalyzer.Report.now', side_effect=defaultEnd)
     def test_checkCurrentDayWithPause(self, now_function):
         logs = LogBuilder().start().pause("13:00", "14:00").build()
         report = report = ReportBuilder().build()
-        self.assertEqual(logAnalyzer.Report().report(logs), report)
+        self.assertEqual(logAnalyzer.Report(logs).report(), report)
 
 if __name__ == '__main__':
     unittest.main()
