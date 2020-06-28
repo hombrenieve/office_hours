911886b6fe096f5eaf53c9165d60ffc4275ede62
Pablo Rebaque <pablo.rebaque.diaz@ericsson.com>
2020-06-28 16:59:00 +0200

    Convenient calculated fields

--- backend/logger/models.py
+++ backend/logger/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 class Session(models.Model):
     user = models.CharField(max_length=64,default='user')
-    start = models.DateTimeField(auto_now=True)
+    start = models.DateTimeField(default=timezone.now)
     end = models.DateTimeField(default=None, blank=True, null=True)
+
+    @property
+    def total(self):
+        return self.end - self.start if (self.end != None) else None
+    
+
 
 class Log(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
-    start = models.DateTimeField(auto_now=True)
+    start = models.DateTimeField(default=timezone.now)
     end = models.DateTimeField(default=None, blank=True, null=True)
+
+    @property
+    def elapsed(self):
+        return self.end - self.start if (self.end != None) else None
