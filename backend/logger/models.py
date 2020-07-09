6cc23729e7f03c76615b602bc7cbe536457fc050
Hombrenieve <pablo.rebaque.diaz@ericsson.com>
2020-07-10 00:35:38 +0200

    Calculate session summary in detailed view

--- backend/logger/models.py
+++ backend/logger/models.py
@@ -1,25 +1,42 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 def deltaToStr(tdelta):
     hours, rem = divmod(tdelta.seconds, 3600)
     minutes, rem = divmod(rem, 60)
     return "{:02d}:{:02d}".format(hours, minutes)
 
+def elapsed(holder):
+    return holder.end - holder.start if (holder.end != None) else timezone.now() - holder.start
+
 class Session(models.Model):
     user = models.CharField(max_length=64,default='user')
     start = models.DateTimeField(default=timezone.now)
     end = models.DateTimeField(default=None, blank=True, null=True)
 
-    @property
-    def total(self):
-        return deltaToStr(self.end - self.start if (self.end != None) else timezone.now() - self.start)
+    def elapsed(self):
+        return elapsed(self)
+    
+    def working(self):
+        delta = timedelta()
+        for l in self.log_set.all():
+            delta += l.elapsed()
+        return delta
+    
+    def resting(self):
+        return self.elapsed() - self.working()
+    
+    def __str__(self):
+        return "Session id(%s) Total(%s) Working(%s) Resting(%s)" % \
+            (self.pk, deltaToStr(self.elapsed()), deltaToStr(self.working()), \
+                deltaToStr(self.resting()))
+
 
 class Log(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     start = models.DateTimeField(default=timezone.now)
     end = models.DateTimeField(default=None, blank=True, null=True)
 
-    @property
     def elapsed(self):
-        return self.end - self.start if (self.end != None) else None
+        return elapsed(self)
