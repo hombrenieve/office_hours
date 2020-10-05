a6e8928a8bd29686f7cd036b19adcac55037c343
Hombrenieve <pablo.rebaque.diaz@ericsson.com>
2020-10-06 00:36:12 +0200

    Add event logic

--- working_hours/src/models.rs
+++ working_hours/src/models.rs
@@ -1,9 +1,9 @@
 pub mod session {
-    use std::time::SystemTime;
+    use std::time::{SystemTime, Duration};
     type Moment = SystemTime;
 
     pub struct Session {
-        start: Option<Moment>,
+        start: Moment,
         end: Option<Moment>,
         events: Vec<Event>
     }
@@ -16,15 +16,39 @@ pub mod session {
     }
 
     impl Session {
-        pub fn new(event: Event) -> Session {
-            if let Event::Create(moment) = event {
-                Session {
-                    start: Some(moment),
-                    end: None,
-                    events: vec![Event::Create(moment)]
+        pub fn new(moment: Moment) -> Session {
+            Session {
+                start: moment,
+                end: None,
+                events: vec![Event::Create(moment)]
+            }
+        }
+        pub fn event(&mut self, an_event: Event) {
+            if let Some(last_event) = self.events.last() {
+                match an_event {
+                    Event::Lock(_) => match last_event {
+                        Event::Unlock(_) | Event::Create(_) => self.events.push(an_event),
+                        _ => ()
+                    },
+                    Event::Unlock(_) => match last_event {
+                        Event::Lock(_) => self.events.push(an_event),
+                        _ => ()
+                    },
+                    Event::Close(moment) => {
+                        self.events.push(an_event);
+                        self.end = Some(moment);
+                    },
+                    _ => ()
                 }
-            } else {
-                panic!("Can't use other event than create to initialize session");
+            }
+        }
+        pub fn get_total(&self) -> Option<Duration> {
+            match self.end {
+                Some(end_time) => match end_time.duration_since(self.start) {
+                    Ok(elapsed) => Some(elapsed),
+                    _ => None
+                }
+                _ => None
             }
         }
     }
@@ -33,17 +57,25 @@ pub mod session {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::SystemTime;
+    use std::time::{SystemTime, Duration};
     use session::*;
 
-    #[test]
-    fn check_session_is_correctly_built() {
-        let ses = Session::new(Event::Create(SystemTime::now()));
+    fn calc_duration(minutes: u32) -> (SystemTime, SystemTime, Duration) {
+        let duration = Duration::from_secs((minutes*60) as u64);
+        let now = SystemTime::now();
+        (now, now+duration, duration)
     }
 
     #[test]
-    #[should_panic]
-    fn session_cant_be_built_with_other_event() {
-        let ses = Session::new(Event::Lock(SystemTime::now()));
+    fn check_session_is_correctly_built() {
+        let ses = Session::new(SystemTime::now());
+    }
+
+    #[test]
+    fn close_event_collect_total() {
+        let (start, end, duration) = calc_duration(90);
+        let mut ses = Session::new(start);
+        ses.event(Event::Close(end));
+        assert_eq!(ses.get_total(), Some(duration));
     }
 }
