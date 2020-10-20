1fc1a3389abacf1f2c0be2d82266900c3db23120
hombrenieve <hombrenieve@gmail.com>
2020-10-20 22:40:28 +0200

    Add state to rocket

--- working_hours/src/main.rs
+++ working_hours/src/main.rs
@@ -5,38 +5,68 @@ mod models;
 pub use models::session::*;
 
 #[macro_use] extern crate rocket;
+use rocket::State;
+use std::sync::{Arc, Mutex};
 
-mod main {
-    use super::*;
+struct SessionState {
+    sessions: Arc<Mutex<Vec<Session>>>
+}
 
-    pub static SESSIONS: Vec<Session> = vec![];
+impl SessionState {
+    fn new(sessions: Vec<Session>) -> SessionState {
+        SessionState {
+            sessions: Arc::new(Mutex::new(sessions))
+        }
+    }
+
+    fn add_session(self, session: Session) {
+        let mut sessions = self.sessions.lock().unwrap();
+        sessions.push(session);
+    }
+
+    fn len(&self) -> usize {
+        self.sessions.lock().unwrap().len()
+    }
 }
 
 #[get("/sessions")]
-fn get_sessions() -> String {
-    format!("The number of sessions is {}", main::SESSIONS.len())
+fn get_sessions(session_state: State<SessionState>) -> String {
+    format!("The number of sessions is {}", session_state.len())
 }
 
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![get_sessions])
+fn rocket(state: SessionState) -> rocket::Rocket {
+    rocket::ignite()
+        .manage(state)
+        .mount("/", routes![get_sessions])
 }
 
 fn main() {
-    rocket().launch();
+    rocket(SessionState::new(vec![])).launch();
 }
 
 
 #[cfg(test)]
 mod test {
     use super::rocket;
+    use super::*;
     use rocket::local::Client;
     use rocket::http::Status;
 
     #[test]
-    fn test_get_sessions() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
+    fn test_get_sessions_no_session() {
+        let client = Client::new(rocket(SessionState::new(vec![]))).expect("valid rocket instance");
         let mut response = client.get("/sessions").dispatch();
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.body_string(), Some("The number of sessions is 0".into()));
     }
+
+    #[test]
+    fn test_get_sessions_one_session() {
+        let sessions = vec![Session::new(chrono::Local::now())];
+        let client = Client::new(rocket(SessionState::new(sessions))).expect("valid rocket instance");
+        let mut response = client.get("/sessions").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("The number of sessions is 1".into()));
+    }
+
 }
