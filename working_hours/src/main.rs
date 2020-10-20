87fe1ffc7afb86de1aff66d5740cb1d535e4fbc7
hombrenieve <hombrenieve@gmail.com>
2020-10-20 23:46:55 +0200

    Add creation of session
    Decision: Use iso 8601 as date format
       Ex: 1996-12-19T16:39:57-08:00

--- working_hours/src/main.rs
+++ working_hours/src/main.rs
@@ -7,6 +7,7 @@ pub use models::session::*;
 #[macro_use] extern crate rocket;
 use rocket::State;
 use std::sync::{Arc, Mutex};
+use rocket::http::RawStr;
 
 struct SessionState {
     sessions: Arc<Mutex<Vec<Session>>>
@@ -19,7 +20,7 @@ impl SessionState {
         }
     }
 
-    fn add_session(self, session: Session) {
+    fn add_session(&self, session: Session) {
         let mut sessions = self.sessions.lock().unwrap();
         sessions.push(session);
     }
@@ -32,12 +33,25 @@ impl SessionState {
 #[get("/sessions")]
 fn get_sessions(session_state: State<SessionState>) -> String {
     format!("The number of sessions is {}", session_state.len())
+
+}
+
+use rocket::response::status::BadRequest;
+
+#[get("/sessions/new?<start>")]
+fn new_session(session_state: State<SessionState>, start: &RawStr) -> Result<String, BadRequest<String>> {
+    if let Ok(session_start) = chrono::DateTime::parse_from_rfc3339(start.as_str()) {
+        session_state.add_session(Session::new(chrono::DateTime::from(session_start)));
+        Ok(format!("{{ id: {} }}", session_state.len()-1))
+    } else {
+        Err(BadRequest(Some("Unknown date format".into())))
+    }
 }
 
 fn rocket(state: SessionState) -> rocket::Rocket {
     rocket::ignite()
         .manage(state)
-        .mount("/", routes![get_sessions])
+        .mount("/", routes![get_sessions, new_session])
 }
 
 fn main() {
@@ -69,4 +83,11 @@ mod test {
         assert_eq!(response.body_string(), Some("The number of sessions is 1".into()));
     }
 
+    #[test]
+    fn test_get_sessions_new_session() {
+        let client = Client::new(rocket(SessionState::new(vec![]))).expect("valid rocket instance");
+        let mut response = client.get("/sessions/new?start=2014-05-02T20:39:57+01:00").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("{ id: 0 }".into()));
+    }
 }
